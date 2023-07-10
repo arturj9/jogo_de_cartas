05e217a59a97cf3e84e514ae609127e6d881c683
Ryan Gomes <ryangomeslima@hotmail.com>
2023-07-10 17:59:34 -0300

    Add files via upload

---
+++ src/view/Main.java
@@ -0,0 +1,24 @@
+package view;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+import controller.Rodada;
+import model.LerCartas;
+import model.RodadasDAO;
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Janela();
+//		//Testando o banco de dados
+//		RodadasDAO teste = new RodadasDAO();
+//		teste.inserir(null);
+		//TelaInicial ti = new TelaInicial();
+		
+	}
+	
+}
